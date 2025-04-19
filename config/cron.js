6c76412e951b3bf1002ccc099827cdffa08bf67f
Anikwe Abuchi Sebastine <anikwesebastine@gmail.com>
2025-04-18 17:15:33 -0700

    time Adjustment

--- config/cron.js
+++ config/cron.js
@@ -2,7 +2,7 @@ const cron = require('cron')
 const sendBirthdayEmails = require('../utils/mailer')
 
 // Create cron job that runs daily at 7 AM
-const birthdayJob = new cron.CronJob('59 16 * * *', async () => {
+const birthdayJob = new cron.CronJob('0 7 * * *', async () => {
   console.log('Running birthday check...')
   await sendBirthdayEmails()
   console.log('Birthday check completed')
